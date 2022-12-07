c4d25571f23789ee31244ab7d078470575f9d4c3
Richard Ouma <richard.ouma@student.moringaschool.com>
2022-12-07 09:50:48 +0300

    Adding the navbar

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Login from './LoginForm';
 import SignUpForm from './SignUpForm';
 import Footer from './Footer';
+import NavBar from './NavaBar';
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Login/>
         <SignUpForm/>
         <Footer/>
+        <NavBar/>
 
 
     </div>
