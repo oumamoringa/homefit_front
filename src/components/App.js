902b45443a074e95342a604cab66e3b2cee77934
Richard Ouma <richard.ouma@student.moringaschool.com>
2022-12-07 10:21:20 +0300

    linking the components and pages

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,76 @@
-import React from 'react';
-// import 'bootstrap/dist/css/bootstrap/Container'
-import Login from './LoginForm';
-import SignUpForm from './SignUpForm';
-import Footer from './Footer';
-import NavBar from './NavaBar';
 
+import { useEffect, useState,React } from 'react';
+import {Routes, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+// import '../styles/App.css';
+import NavBar from '../components/NavaBar';
+import Footer from './Footer';
+import Login from '../pages/Login';
+import NewTutorial from '../pages/NewTutorial';
+import Trainers from '../pages/Trainers';
+import YogaTutorials from '../pages/YogaTutorials';
+import Trainees from '../pages/Trainees';
+import Profile from '../pages/Profile';
 
 function App() {
+  const [user, setUser] = useState(null);
+  const [trainers, setTrainers] = useState([]);
+
+
+  // useEffect(() => {
+  //   fetch('/me')
+  //     .then(r => {
+  //       if (r.ok) {
+  //         r.json().then(userObj => setUser(userObj));
+  //       }
+  //     });
+  // }, []);
+
+  useEffect(() => {
+    // auto-login
+    fetch("/me").then((r) => {
+      if (r.ok) {
+        r.json().then((user) => setUser(user));
+      }
+    });
+  }, []);
+
+
+
+
+  useEffect(() => {
+    // fetch("/trainers")
+    //   .then((r) => r.json())
+    //   .then( (trainers)=>setTrainers);
+
+      fetch("/trainers").then((r) => {
+        if (r.ok) {
+          r.json().then((trainers) => setTrainers(trainers));
+        }
+      });
+
+  }, []);
+
+
+   if (!user) return <Login onLogin={setUser} />;
+  
   return (
-    <div className="App">
-        <Login/>
-        <SignUpForm/>
-        <Footer/>
-        <NavBar/>
+    <>
+      <NavBar user={user} setUser={setUser} />
+      <Container style={{background:"#dede"}} >
+      <Routes>
+        <Route exact  path="/add-tutorial"  element={<NewTutorial setTrainers={setTrainers} trainers={trainers}  user={user}   />}/>
+        <Route path="/"   element={<YogaTutorials/>}/>
+        <Route path="/yoga-tutorials"  element={<YogaTutorials />}/>
+        <Route path="/trainers"    element={<Trainers trainers={trainers} setTrainers={setTrainers}/>}/>
+        <Route path="/trainees"    element={<Trainees/>}/>
+        <Route path="/profile" element={<Profile user={user} />}/>
+      </Routes>
+      <Footer/>
+      </Container>
 
-
-    </div>
+    </>
   );
 }
 
