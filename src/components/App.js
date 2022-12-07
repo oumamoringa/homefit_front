e62e60f3b2b0a58f1cf66127fa7ea5ff2b3fc979
Richard Ouma <richard.ouma@student.moringaschool.com>
2022-12-07 09:33:36 +0300

    Adding the sign up form

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 // import 'bootstrap/dist/css/bootstrap/Container'
 import Login from './LoginForm';
+import SignUpForm from './SignUpForm';
 
 
 function App() {
   return (
     <div className="App">
         <Login/>
+        <SignUpForm/>
+
 
     </div>
   );
