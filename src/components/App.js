013e064b09564c4f165868edfdd58553d1434a77
Richard Ouma <richard.ouma@student.moringaschool.com>
2022-12-07 08:25:37 +0300

    creating the main components

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import 'bootstrap/dist/css/bootstrap/Container'
+import Login from './LoginForm';
+
+
+function App() {
+  return (
+    <div className="App">
+        <Login/>
+
+    </div>
+  );
+}
+
+export default App;
